7dc3949fb099b2e4695e046769390d754c0784ad
barthimaeus <barthimaeus@web.de>
2012-12-06 12:10:47 +0100

    Added data file

--- dbscan.hs
+++ dbscan.hs
@@ -1,3 +1,5 @@
+module Main where
+
 import Data.Char
 import Data.List
 import Text.ParserCombinators.Parsec
@@ -5,35 +7,39 @@ import Text.ParserCombinators.Parsec
 data Point a = Pt a a deriving (Show, Eq)
 manhattan (Pt x y) (Pt x' y') = abs (x' - x) + abs (y' - y)
 
-main = interact $ show . simplescan . parsepoints
+main =  do  input <- getContents
+            let points = case   parse parsepoints "stdin" input of 
+                                Left err -> error $ "Parse error"
+                                Right result -> result
+            print points
+            print $ simplescan points
+        
 
+parsepoints =
+    do
+        points <- many parsepoint
+        eof :: Parser ()
+        return points
 
-split :: Char -> String -> [String]
-split _ "" = [""]
-split sp (c:cs)
-    | c == sp = "": rest
-    | c == '\n' = "": rest
-    | otherwise = (c : head rest) : tail rest
-    where
-       rest = split sp cs
-
-splitcomma' = split ','
-splitcomma l = filter (\x -> not (x == "")) (splitcomma' l)
-
-makepoints [] = []
-makepoints (x:y:ys) = (Pt (read x::Int) (read y::Int)) : (makepoints ys)
-
-parsepoints = makepoints . splitcomma
+parsepoint =
+    do 
+        x <- many1 digit
+        spaces
+        char ','
+        spaces
+        y <- many1 digit
+        spaces
+        return $ Pt (read x::Int) (read y::Int)
 
 simplescan = dbscan 2 3
 dbscan eps minpts points = filter (not.null) $ map (\p -> (cluster p eps minpts points)) points
-
-region x eps points = filter (\p -> (manhattan x p) <= eps && not (x == p)) points
 
 cluster x eps minpts points 
     | length neighbors < minpts  = []
     | otherwise = x : (expand neighbors eps minpts points [x])
     where neighbors = region x eps points
+
+region x eps points = filter (\p -> (manhattan x p) <= eps && not (x == p)) points
 
 expand [] _ _ _ _ = []
 expand c eps minpts points visited
